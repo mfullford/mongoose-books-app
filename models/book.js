d05f05241bd67587a9389c71f60a187f027a4a23
mfullford <marissafullford@gmail.com>
2017-10-10 10:55:52 -0600

    Saves changes

---
+++ models/book.js
@@ -0,0 +1,14 @@
+//book.js
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var BookSchema = new Schema({
+	title: String,
+	author: String,
+	image: String,
+	realeaseDate: String
+});
+
+var Book = mongoose.model('Book', BookSchema);
+
+module.exports = Book;
--- models/index.js
+++ models/index.js
@@ -1,2 +1,7 @@
+// requires mongoose
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/book-app");
+
+// exports the books AND requires the book
+module.exports.Book = require('./book.js');
+
